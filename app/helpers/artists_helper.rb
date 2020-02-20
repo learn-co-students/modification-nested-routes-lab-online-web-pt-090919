63de4120fb09b350f22c73f262651262c5892df9
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-02-20 11:58:58 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,12 @@
 module ArtistsHelper
+  # display link to edit song if artist empty
+  # display link to artist page if artist not empty
   def display_artist(song)
-    song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+    # song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+    if song.artist.nil? || song.artist_name.empty?
+      link_to("Add Artist", edit_song_path(song))
+    else
+      link_to(song.artist_name, artist_path(song.artist))
+    end
   end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
-
+  def artist_select(song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      # Add hidden field form_for :artist_id when we browse to
+      # /artists/1/songs/new
+      hidden_field_tag "song[aritst_id]", song.artist_id
+    end
+  end
 end
