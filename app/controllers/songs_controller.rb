f69c698a8c8a96704679c10d54241abc921c8583
Lauren Dove <elledoveinc@gmail.com>
2020-02-22 19:36:48 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,21 +25,39 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
-    @song = Song.new(song_params)
+    if params[:artist_id] # im not even sure how or why this worked...Need to ask!
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = Song.new(song_params)
 
     if @song.save
       redirect_to @song
     else
       render :new
     end
+
   end
+end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(artist), alert: "Song not found" if @song.nil?
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
